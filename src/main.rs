4ef0d6694f3c96c1efd927755111dc910ddb80ad
Anton Sapunov <fort.sav.28@gmail.com>
2019-04-23 23:38:52 +0700

    Regex is done

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,18 @@ use std::path::Path;
 use std::process;
 
 fn main() {
-    println!("Hello, world!");
+    let mut vec_with_parsed_lines = Vec::new();
+
     let log = get_args().unwrap_or_else(|err| {
         eprintln!("Problem passing arguments: {}", err);
         process::exit(1);
     });
 
     // TODO: change to unwrap_or_else
-    let file_data_iter = read_file(&log).unwrap();
 
-    //for line in file_data_iter {
-    //    println!("{}", line.unwrap());
-    //}
-    //
-
-    check_regex();
-
-    dbg!(&log);
+    for line in read_file(&log).unwrap() {
+        vec_with_parsed_lines.push(line.unwrap());
+    }
 }
 
 fn get_args() -> std::result::Result<String, &'static str> {
@@ -49,7 +44,7 @@ fn read_file(file_name: &str) -> std::result::Result<Lines<BufReader<File>>, io:
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
-struct LogMessage {
+struct ParsedLine {
     facility: String,
     severity: String,
     timestamp: String,
@@ -58,9 +53,9 @@ struct LogMessage {
 }
 
 //TODO: maybe do I need something more meaningful?
-impl LogMessage {
-    pub fn new() -> LogMessage {
-        LogMessage {
+impl ParsedLine {
+    pub fn new() -> ParsedLine {
+        ParsedLine {
             facility: String::new(),
             severity: String::new(),
             timestamp: String::new(),
@@ -70,37 +65,55 @@ impl LogMessage {
     }
 }
 
-fn check_regex() {
-    let reg = Regex::new(r"<(\d{2})(\d{1})>(Nov\s\d+\s\d{2}:\d{2}:\d{2})\s(.+?):\s(.*)").unwrap();
+fn parse_line(line: &str) -> ParsedLine {
+    let pattern = Regex::new(
+        r"(?x)
+        <
+        (?P<facility>\d{2})
+        (?P<severity>\d{1})
+        >
+        (?P<timestamp>\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2})\s
+        (?P<source_name>.*)
+        :\s
+        (?P<data>.*)",
+    )
+    .unwrap();
 
-    let log_string = "<166>Nov 13 15:38:01 10.181.233.206 %ASA-6-303002: FTP connection from outside:172.21.173.130/49845 to vos2:10.81.123.19/21, user comersant Stored file 2014-11-13_15-33-29---1---megafon-out.wav";
-
-    for cap in reg.captures_iter(log_string) {
-        println!(
-            "1: {}\n 2: {}\n 3: {}\n 4: {}\n 5: {}\n",
-            &cap[1], &cap[2], &cap[3], &cap[4], &cap[5]
-        );
-    }
+    let parsed_line: ParsedLine = from_captures(&pattern, line).unwrap();
+    parsed_line
 }
 
+//TODO: fix test, it must be normal unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn check_recap() {
-        let pattern = Regex::new(r#"(?P<facility>\S+)\s(?P<severity>\S+)\s(?P<timestamp>\S+)\s(?P<source_name>\S+)\s(?P<data>\S+)"#).unwrap();
+        let pattern = Regex::new(
+            r"(?x)
+            <
+            (?P<facility>\d{2})
+            (?P<severity>\d{1})
+            >
+            (?P<timestamp>\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2})\s
+            (?P<source_name>.*)
+            :\s
+            (?P<data>.*)",
+        )
+        .unwrap();
 
-        let example: LogMessage = from_captures(&pattern, "hello there who is here").unwrap();
+        let log_string = "<166>Nov 13 15:38:01 10.181.233.206 %ASA-6-303002: FTP connection";
+        let example: ParsedLine = from_captures(&pattern, log_string).unwrap();
 
         assert_eq!(
             example,
-            LogMessage {
-                facility: "hello".into(),
-                severity: "there".into(),
-                timestamp: "who".into(),
-                source_name: "is".into(),
-                data: "here".into(),
+            ParsedLine {
+                facility: "16".into(),
+                severity: "6".into(),
+                timestamp: "Nov 13 15:38:01".into(),
+                source_name: "10.181.233.206 %ASA-6-303002".into(),
+                data: "FTP connection".into(),
             }
         );
     }
